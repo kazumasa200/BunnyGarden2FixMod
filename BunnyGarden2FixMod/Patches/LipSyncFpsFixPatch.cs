585b98456f45b4e1be5bd899b6fd5cc122322b2b
pasotako <tatsuya.osm@gmail.com>
2026-05-06 17:27:31 +0900

    fix: フレームレートの違いを厳密に反映

--- BunnyGarden2FixMod/Patches/LipSyncFpsFixPatch.cs
+++ BunnyGarden2FixMod/Patches/LipSyncFpsFixPatch.cs
@@ -35,28 +35,49 @@ public static class LipSyncFpsFixPatch
         var originalMethod = AccessTools.Method(typeof(Mathf), nameof(Mathf.Lerp), new[] { typeof(float), typeof(float), typeof(float) });
         var replacedMethod = AccessTools.Method(typeof(LipSyncFpsFixPatch), nameof(CustomLerp));
 
-        bool patched = false;
+        var GetFrequency = typeof(AudioClip).GetProperty("frequency").GetGetMethod();
+        var GetFPSMethod = AccessTools.Method(typeof(LipSyncFpsFixPatch), nameof(GetFPS));
+
+        int patched = 0;
         for (int i = 0; i < codes.Count; i++)
         {
+            // this.m_clip.frequency / 60 の書き換え
+            if (codes[i].Calls(GetFrequency) &&
+                i+1 < codes.Count && codes[i+1].LoadsConstant(60) &&
+                i+2 < codes.Count && codes[i+2].opcode == OpCodes.Div)
+            {
+                codes[i+1] = new CodeInstruction(OpCodes.Call, GetFPSMethod);
+                patched++;
+                continue;
+            }
+
+            // lerpの書き換え
             if (codes[i].opcode == OpCodes.Call && codes[i].operand is MethodInfo method && method == originalMethod)
             {
                 codes[i].operand = replacedMethod;
-                patched = true;
+                patched++;
+                continue;
             }
         }
 
-        if(!patched)
+        if(patched != 2)
         {
             PatchLogger.LogError("[LipSyncFpsFixPatch] 修正対象のパターンが見つかりませんでした。ゲームのアップデートでパッチが機能していない可能性があります。");
         }
         return codes;
     }
 
-    private static float CustomLerp(float weight, float num, float t)
+    private static float CustomLerp(float a, float b, float t)
+    {
+        float custom_t = 1.0f - Mathf.Pow(1.0f - t, 60f / (float)GetFPS());
+        return Mathf.Lerp(a, b, custom_t);
+    }
+
+    private static int GetFPS()
     {
         int AppliedFrameRate = Application.targetFrameRate;
-        float FPS = AppliedFrameRate <= 0 ? 60f : (float)AppliedFrameRate;
-        float customSpeed = 0.35f * 60f / FPS;
-        return Mathf.Lerp(weight, num, customSpeed);
+        int FPS = AppliedFrameRate <= 0 ? 60 : AppliedFrameRate;
+        return FPS;
     }
+
 }
